d56f16aa794f168956a309309ea89b62043801fa
ErnestTabilas <eltabilas@up.edu.ph>
2023-06-03 21:06:40 +0800

    feat: Pending accounts can now be accepted and rejected by the admin and the search functionality alongside with the sorting functionalities (ascending or descending) by student number or name was also added.

--- client/src/pages/admin/PendingAccounts.jsx
+++ client/src/pages/admin/PendingAccounts.jsx
@@ -1,101 +1,157 @@
 import { useUserStore } from "../../store";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { apiAxios } from "../../utils";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 
-import { FaSortAlphaUp } from 'react-icons/fa';
-import { FaSortNumericUp } from 'react-icons/fa';
+import { FaSortAlphaUp, FaSortAlphaDown, FaSortNumericUp, FaSortNumericDown } from 'react-icons/fa';
+import { FaCheck, FaTimes } from 'react-icons/fa';
 
 const fetcher = (url) => apiAxios.get(url).then((res) => res.data);
 
-const printAllPendings = () => {
-    const { data, error, isLoading } = useSWR('/students/pending', fetcher);
-
-    if (error) {
-        console.log(error);
-        return <div>idk why this failed.</div>;
-    }
-    if (isLoading) return <div>Loading...</div>;
-    const pendings = data.data;
-
-    const [selectedStudent, setSelectedStudent] = useState(null); // State to store the selected student
-
-    const handleAssignAdviser = (student) => {
-        setSelectedStudent(student); // Set the selected student
-    };
-
-    const handleCloseModal = () => {
-        setSelectedStudent(null); // Clear the selected student
-    };
-
-    const handleAssign = () => {
-        // Implement the logic to assign an adviser to the selected student using the selectedStudent object
-        // ...
-        setSelectedStudent(null); // Clear the selected student after assigning the adviser
-    };
-
-    return (
-        <div className="table-container">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th className="text-primary bg-transparent pr-20">Student Number</th>
-                        <th className="text-primary bg-transparent pr-20">Name</th>
-                        <th className="text-primary bg-transparent pr-20">Email</th>
-                        <th className="text-primary bg-transparent pr-20">Actions   </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {pendings.map((pending) => (
-                        <tr key={pending._id}>
-                            <td>{pending.studentNumber}</td>
-                            <td>{`${pending.firstName} ${pending.lastName}`}</td>
-                            <td>{pending.email}</td>
-                            < hr/>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-}
-
 const PendingAccountsPage = () => {
-    const user = useUserStore((state) => state.user);
+  const user = useUserStore((state) => state.user);
 
-    if (user.userType != "admin")
-        throw new Response("Not Found", { status: 404 });
+  if (user.userType !== "admin") {
+    throw new Response("Not Found", { status: 404 });
+  }
 
-    return (
-        <div>
-            <h1 className="text-4xl font-bold text-black mb-6 py-0">Pending Accounts</h1>
-            <div className="flex items-center mb-4">
-              <input
-                type="text"
-                placeholder="Search an account"
-                className="px-4 py-3 border border-gray-300 rounded-l focus:outline-none"
-              />
-              <button className="bg-primary text-white px-4 py-2 rounded-r">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-8 w-5"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <circle cx="8" cy="8" r="5" />
-                  <path d="M15 15l-4.35-4.35" />
-                </svg>
-              </button>
-            </div>
-            <div className="flex space-x-2 text-sm text-red-500 mb-6">
-                <button key='0' className="px-5 py-1 bg-transparent text-primary rounded">Student Number <FaSortNumericUp /></button>
-                <button key='1' className="px-5 py-1 bg-transparent text-primary rounded">Name <FaSortAlphaUp /></button>
-            </div>
-            {printAllPendings()}
-        </div>
-    );
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  
+  const handleSort = (key) => {
+    if (sortBy === key) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortBy(key);
+      setSortOrder('asc');
+    }
+  };
+
+  const handleAccept = async (studentNumber) => {
+    try {
+      await apiAxios.post('/student/verify', { studentNumber });
+      mutate('/students/pending'); // Refresh the list after accepting
+    } catch (error) {
+      // Handle error
+    }
+  };
+
+  const handleReject = async (studentNumber) => {
+    try {
+      await apiAxios.post('/student/reject', { studentNumber });
+      mutate('/students/pending'); // Refresh the list after rejecting
+    } catch (error) {
+      // Handle error
+    }
+  };
+
+  const { data, error } = useSWR('/students/pending', fetcher);
+
+  if (error) {
+    console.log(error);
+    return <div>idk why this failed.</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  let pendings = data.data;
+
+  const filteredPendings = pendings.filter((pending) => {
+    const fullName = `${pending.firstName} ${pending.lastName}`;
+    return fullName.toLowerCase().includes(searchKeyword.toLowerCase());
+  });
+
+  const sortedPendings = sortBy
+    ? filteredPendings.sort((a, b) => {
+        if (sortBy === 'studentNumber') {
+          return sortOrder === 'asc' ? a[sortBy] - b[sortBy] : b[sortBy] - a[sortBy];
+        } else {
+          return sortOrder === 'asc' ? a[sortBy].localeCompare(b[sortBy]) : b[sortBy].localeCompare(a[sortBy]);
+        }
+      })
+    : filteredPendings;
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-black mb-6 py-0">Pending Accounts</h1>
+      <div className="flex items-center mb-4">
+        <input
+          type="text"
+          placeholder="Search an account"
+          className="px-4 py-3 border border-gray-300 rounded-l focus:outline-none"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
+        <button className="bg-primary text-white px-4 py-2 rounded-r">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-5"
+            viewBox="0 0 20 20"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <circle cx="8" cy="8" r="5" />
+            <path d="M15 15l-4.35-4.35" />
+          </svg>
+        </button>
+      </div>
+      <div className="flex space-x-2 text-sm text-red-500 mb-6">
+        <button key='0' className="px-5 py-1 bg-transparent text-primary rounded">
+          <button
+            className="sorting-button"
+            onClick={() => handleSort('studentNumber')}
+          >
+            Student Number {sortBy === 'studentNumber' && (
+              sortOrder === 'asc' ? <FaSortNumericUp /> : <FaSortNumericDown />
+            )}
+          </button>
+        </button>
+        <button key='1' className="px-5 py-1 bg-transparent text-primary rounded">
+          <button
+            className="sorting-button"
+            onClick={() => handleSort('firstName')}
+          >
+            Name {sortBy === 'firstName' && (
+              sortOrder === 'asc' ? <FaSortAlphaUp /> : <FaSortAlphaDown />
+            )}
+          </button>
+        </button>
+      </div>
+      <div className="table-container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="text-primary bg-transparent pr-20">Student Number</th>
+              <th className="text-primary bg-transparent pr-20">Name</th>
+              <th className="text-primary bg-transparent pr-20">Email</th>
+              <th className="text-primary bg-transparent pr-20">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedPendings.map((pending) => (
+              <tr key={pending._id}>
+                <td>{pending.studentNumber}</td>
+                <td>{`${pending.firstName} ${pending.lastName}`}</td>
+                <td>{pending.email}</td>
+                <td>
+                  <button className="p-1 btn-primary mr-4 rounded-full action-button accept-button" onClick={() => handleAccept(pending.studentNumber)}>
+                    <FaCheck />
+                  </button>
+                  <button className="p-1 btn-primary rounded-full action-button reject-button" onClick={() => handleReject(pending.studentNumber)}>
+                    <FaTimes />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default PendingAccountsPage;
--- client/src/pages/admin/StudentAccounts.jsx
+++ client/src/pages/admin/StudentAccounts.jsx
@@ -1,145 +1,174 @@
 import { useUserStore } from "../../store";
 import { useState } from "react";
 import { apiAxios } from "../../utils";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 
 import { FaSortAlphaUp } from 'react-icons/fa';
 import { FaSortNumericUp } from 'react-icons/fa';
 
 const fetcher = (url) => apiAxios.get(url).then((res) => res.data);
 
-const printAllStudents = () => {
-    const { data, error, isLoading } = useSWR("/students", fetcher);
-
-    if (error) {
-        console.log(error);
-        return <div>idk why this failed.</div>;
-    }
-    if (isLoading) return <div>Loading...</div>;
-    const students = data.data;
-
-    const [selectedStudent, setSelectedStudent] = useState(null); // State to store the selected student
-
-    const handleAssignAdviser = (student) => {
-        setSelectedStudent(student); // Set the selected student
-    };
-
-    const handleCloseModal = () => {
-        setSelectedStudent(null); // Clear the selected student
-    };
-
-    const handleAssign = () => {
-        // Implement the logic to assign an adviser to the selected student using the selectedStudent object
-        // ...
-        setSelectedStudent(null); // Clear the selected student after assigning the adviser
-    };
-
-    return (
-        <div className="table-container">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th className="text-primary bg-transparent pr-20">Student Number</th>
-                        <th className="text-primary bg-transparent pr-20">Name</th>
-                        <th className="text-primary bg-transparent pr-20">Email</th>
-                        <th className="text-primary bg-transparent pr-20">Adviser</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {students.map((student) => (
-                        <tr key={student._id}>
-                            <td>{student.studentNumber}</td>
-                            <td>{`${student.firstName} ${student.lastName}`}</td>
-                            <td>{student.email}</td>
-                            <td>
-                                {student.adviser ? (
-                                    student.adviser
-                                ) : (
-                                    <button className="px-2 py-1 bg-transparent text-primary rounded" onClick={() => handleAssignAdviser(student)}>Assign</button>
-                                )}
-                            </td>
-                            < hr/>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-
-            {selectedStudent && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2 className="text-primary">Assign Adviser</h2>
-                        <div>
-                            <select className="dropdown" placeholder="Search an adviser">
-                                {/* Render the list of advisers here */}
-                            </select>
-                        </div>
-                        <div>
-                            <button className="btn btn-primary" onClick={handleAssign}>Assign</button>
-                        </div>
-                        <button className="btn btn-secondary" onClick={handleCloseModal}>Close</button>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
-
 const StudentAccountsPage = () => {
-    const user = useUserStore((state) => state.user);
+  const user = useUserStore((state) => state.user);
 
-    if (user.userType !== "admin")
-        throw new Error("Not Found");
+  if (user.userType !== "admin") {
+    throw new Error("Not Found");
+  }
 
-    const handleAssignAdviser = () => {
-        // Implement the logic to handle the 'Assign' button click event here
-        // This function will be called when the 'Assign' button in the pop-up window is clicked
-    };
+  const { data, error } = useSWR("/students", fetcher);
+  const students = data?.data || [];
 
-    return (
-        <div>
-            <h1 className="text-4xl font-bold text-black mb-6 py-0">
-                Student Accounts
-            </h1>
-            <div className="flex items-center mb-4">
-                <input
-                    type="text"
-                    placeholder="Search a student"
-                    className="px-4 py-3 border border-gray-300 rounded-l focus:outline-none"
-                />
-                <button className="bg-primary text-white px-4 py-2 rounded-r">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-8 w-5"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
+  const [selectedStudent, setSelectedStudent] = useState(null); // State to store the selected student
+  const [searchQuery, setSearchQuery] = useState(""); // State to store the search query
+  const [sortBy, setSortBy] = useState(""); // State to store the sort field
+
+  const handleAssignAdviser = (student) => {
+    setSelectedStudent(student); // Set the selected student
+  };
+
+  const handleCloseModal = () => {
+    setSelectedStudent(null); // Clear the selected student
+  };
+
+  const handleAssign = () => {
+    // Implement the logic to assign an adviser to the selected student using the selectedStudent object
+    // ...
+    setSelectedStudent(null); // Clear the selected student after assigning the adviser
+  };
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value); // Update the search query state
+  };
+
+  const handleSort = (field) => {
+    setSortBy(field); // Update the sort field state
+  };
+
+  // Filter the students based on the search query
+  const filteredStudents = students.filter((student) =>
+    student.firstName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  // Sort the students based on the sort field
+  const sortedStudents = filteredStudents.sort((a, b) => {
+    if (sortBy === "studentNumber") {
+      return a.studentNumber.localeCompare(b.studentNumber);
+    } else if (sortBy === "name") {
+      return a.firstName.localeCompare(b.firstName);
+    }
+    return 0;
+  });
+
+  if (error) {
+    console.log(error);
+    return <div>idk why this failed.</div>;
+  }
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-black mb-6 py-0">
+        Student Accounts
+      </h1>
+      <div className="flex items-center mb-4">
+        <input
+          type="text"
+          placeholder="Search a student"
+          className="px-4 py-3 border border-gray-300 rounded-l focus:outline-none"
+          value={searchQuery}
+          onChange={handleSearch}
+        />
+        <button className="bg-primary text-white px-4 py-2 rounded-r">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-5"
+            viewBox="0 0 20 20"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <circle cx="8" cy="8" r="5" />
+            <path d="M15 15l-4.35-4.35" />
+          </svg>
+        </button>
+      </div>
+      <div className="flex space-x-5 text-sm text-red-500 mb-6">
+        <button
+          key="0"
+          className="px-2 py-1 bg-transparent text-primary rounded"
+          onClick={() => handleSort("studentNumber")}
+        >
+          Student Number
+          <FaSortNumericUp />
+        </button>
+        <button
+          key="1"
+          className="px-2 py-1 bg-transparent text-primary rounded"
+          onClick={() => handleSort("name")}
+        >
+          Name
+          <FaSortAlphaUp />
+        </button>
+      </div>
+      <button className="btn btn-primary mb-4">UPLOAD A CSV</button>
+
+      <div className="table-container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="text-primary bg-transparent pr-20">
+                Student Number
+              </th>
+              <th className="text-primary bg-transparent pr-20">Name</th>
+              <th className="text-primary bg-transparent pr-20">Email</th>
+              <th className="text-primary bg-transparent pr-20">Adviser</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedStudents.map((student) => (
+              <tr key={student._id}>
+                <td>{student.studentNumber}</td>
+                <td>{`${student.firstName} ${student.lastName}`}</td>
+                <td>{student.email}</td>
+                <td>
+                  {student.adviser ? (
+                    student.adviser
+                  ) : (
+                    <button
+                      className="px-2 py-1 bg-transparent text-primary rounded"
+                      onClick={() => handleAssignAdviser(student)}
                     >
-                        <circle cx="8" cy="8" r="5" />
-                        <path d="M15 15l-4.35-4.35" />
-                    </svg>
+                      Assign
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {selectedStudent && (
+          <div className="modal">
+            <div className="modal-content">
+              <h2 className="text-primary">Assign Adviser</h2>
+              <div>
+                <select className="dropdown" placeholder="Search an adviser">
+                  {/* Render the list of advisers here */}
+                </select>
+              </div>
+              <div>
+                <button className="btn btn-primary" onClick={handleAssign}>
+                  Assign
                 </button>
+              </div>
+              <button className="btn btn-secondary" onClick={handleCloseModal}>
+                Close
+              </button>
             </div>
-            <div className="flex space-x-5 text-sm text-red-500 mb-6">
-                <button
-                    key="0"
-                    className="px-2 py-1 bg-transparent text-primary rounded"
-                >
-                    Student Number
-                    <FaSortNumericUp />
-                </button>
-                <button
-                    key="1"
-                    className="px-2 py-1 bg-transparent text-primary rounded"
-                >
-                    Name
-                    <FaSortAlphaUp />
-                </button>
-            </div>
-            <button className="btn btn-primary mb-4">UPLOAD A CSV</button>
-            {printAllStudents()}
-        </div>
-    );
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default StudentAccountsPage;
