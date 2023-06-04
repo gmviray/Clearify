2a201ddf8a23643af4c6fe19c57f42114c63ef2a
ErnestTabilas <eltabilas@up.edu.ph>
2023-06-04 21:08:18 +0800

    feat: Added the create account functionality in the approver accounts' view.

--- client/src/pages/admin/ApproverAccounts.jsx
+++ client/src/pages/admin/ApproverAccounts.jsx
@@ -22,6 +22,7 @@ const ApproverAccountsPage = () => {
   const [sortDirection, setSortDirection] = useState("asc");
   const [editApprover, setEditApprover] = useState(null);
   const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+  const [createModalIsOpen, setCreateModalIsOpen] = useState(false);
 
   const { register, handleSubmit, reset } = useForm();
 
@@ -65,11 +66,25 @@ const ApproverAccountsPage = () => {
     reset(editApprover);
   }, [editApprover, reset]);
 
+  const handleCreateModalOpen = () => {
+    setCreateModalIsOpen(true);
+  };
+  
+  const onCreateSubmit = async (formData) => {
+    try {
+      await apiAxios.post("/sign-up/approver", formData);
+      setCreateModalIsOpen(false);
+      mutate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   if (error) return <div>idk why this failed</div>;
   if (isLoading) return <div>Loading...</div>;
 
   const filteredApprovers = approvers.filter((approver) =>
-    approver.username.toLowerCase().includes(searchQuery.toLowerCase())
+    approver.firstName.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   const sortedApprovers = filteredApprovers.sort((a, b) => {
@@ -119,7 +134,9 @@ const ApproverAccountsPage = () => {
           ) : null}
         </button>
       </div>
-      <button className="btn btn-primary">CREATE ACCOUNT</button>
+      <button className="btn btn-primary" onClick={handleCreateModalOpen}>
+        CREATE ACCOUNT
+      </button>
       <div className="table-container">
         <table className="table">
           <thead>
@@ -194,18 +211,18 @@ const ApproverAccountsPage = () => {
                   <input type="text" className="border-2 rounded-md py-2" id="firstName" {...register("firstName", {required: true,})} defaultValue={editApprover?.firstName} />
                 </div>
                 <div className="px-8">
-                  <label htmlFor="lastName" className="py-2 block mb-1 text-primary font-bold">Last Name</label>
-                  <input type="text" className="border-2 rounded-md py-2" id="lastName" {...register("lastName", {required: true,})} defaultValue={editApprover?.lastName} />
+                  <label htmlFor="middleName" className="py-2 block mb-1 text-primary font-bold">Middle Name</label>
+                  <input type="text" className="border-2 rounded-md py-2" id="middleName" {...register("middleName", {required: true,})} defaultValue={editApprover?.middleName} />
                 </div>
               </div>
               <div className="flex mb-4">
                 <div className="mr-4">
-                  <label htmlFor="middleName" className="py-2 block mb-1 text-primary font-bold">Middle Name</label>
-                  <input type="text" className="border-2 rounded-md py-2" id="middleName" {...register("middleName", {required: true,})} defaultValue={editApprover?.middleName} />
+                  <label htmlFor="lastName" className="py-2 block mb-1 text-primary font-bold">Last Name</label>
+                  <input type="text" className="border-2 rounded-md py-2" id="lastName" {...register("lastName", {required: true,})} defaultValue={editApprover?.lastName} />
                 </div>
                 <div className="px-8">
                   <label htmlFor="password" className="py-2 block mb-1 text-primary font-bold">Password</label>
-                  <input type="password" className="border-2 rounded-md py-2" id="password" {...register("password", {required: true,})} defaultValue={editApprover?.password} />
+                  <input type="password" className="border-2 rounded-md py-2" placeholder="Enter password" id="password" {...register("password", {required: true,})} defaultValue={editApprover?.password} />
                 </div>
               </div>
               <div className="flex items-center mb-4 py-2">
@@ -219,6 +236,73 @@ const ApproverAccountsPage = () => {
           </div>
         </div>
       )}
+      {createModalIsOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div className="bg-white rounded-lg p-6 w-4/12">
+            <div className="flex justify-end">
+                <button
+                className="btn-primary text-white p-1 rounded-full absolute top-0 right-0 relative"
+                onClick={() => setCreateModalIsOpen(false)}
+                >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+                </button>
+            </div>
+            <h2 className="text-primary text-xl font-bold mb-4">Create Account</h2>
+            <form onSubmit={handleSubmit(onCreateSubmit)}>
+               <div className="flex mb-4">
+                <div className="mr-4">
+                    <label htmlFor="username" className="py-2 block mb-1 text-primary font-bold">Username</label>
+                    <input type="text" className="border-2 rounded-md py-2" placeholder="Enter a username" id="username" {...register("username", {required: true})} />
+                </div>
+                <div className="px-8">
+                    <label htmlFor="email" className="py-2 block mb-1 text-primary font-bold">UP Mail</label>
+                    <input type="email" className="border-2 rounded-md py-2" placeholder="Enter a UP mail" id="email" {...register("email", {required: true})} />
+                </div>
+              </div>
+              <div className="flex mb-4">
+                <div className="mr-4">
+                  <label htmlFor="firstName" className="py-2 block mb-1 text-primary font-bold">First Name</label>
+                  <input type="text" className="border-2 rounded-md py-2" placeholder="Enter a first name" id="firstName" {...register("firstName", {required: true,})} />
+                </div>
+                <div className="px-8">
+                  <label htmlFor="middleName" className="py-2 block mb-1 text-primary font-bold">Middle Name</label>
+                  <input type="text" className="border-2 rounded-md py-2" placeholder="Enter a middle name" id="middleName" {...register("middleName", {required: false,})} />
+                </div>
+              </div>
+              <div className="flex mb-4">
+                <div className="mr-4">
+                  <label htmlFor="lastName" className="py-2 block mb-1 text-primary font-bold">Last Name</label>
+                  <input type="text" className="border-2 rounded-md py-2" placeholder="Enter a last name" id="lastName" {...register("lastName", {required: true,})} />
+                </div>
+                <div className="px-8">
+                  <label htmlFor="password" className="py-2 block mb-1 text-primary font-bold">Password</label>
+                  <input type="password" className="border-2 rounded-md py-2" placeholder="Enter a password" id="password" {...register("password", {required: true,})} />
+                </div>
+              </div>
+              <div className="flex items-center mb-4 py-2">
+                <input type="checkbox" className="h-5 w-5" id="clearanceOfficer" {...register("clearanceOfficer")} defaultChecked={editApprover?.clearanceOfficer} />
+                <label htmlFor="clearanceOfficer" className="ml-2">Mark/Remove as Clearance Officer</label>
+              </div>
+              <div className="text-center">
+                <button type="submit" className="btn btn-primary w-full h-3">CREATE</button>
+              </div>
+            </form>
+            </div>
+        </div>
+       )}
     </div>
   );
 };
