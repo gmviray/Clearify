6560ee10b7c1082cfda9cd0bc3ea763d1f80f33d
kennethtegrado <kategrado@up.edu.ph>
2023-06-05 20:02:28 +0800

    feat: add connection between endpoints and student

--- client/src/pages/student/ClearanceApplication.jsx
+++ client/src/pages/student/ClearanceApplication.jsx
@@ -1,27 +1,6 @@
 import img from "../../assets/img/clearanceApplication.svg";
 
-import { useSWRConfig } from "swr";
-
-import { useState } from "react";
-
-import { apiAxios } from "../../utils";
-
-const ClearanceApplicationPage = ({ studentNumber }) => {
-    const [link, setLink] = useState("");
-    const { mutate } = useSWRConfig();
-    const applyApplication = async () => {
-        try {
-            await apiAxios.post("/application", {
-                studentNumber,
-                link,
-            });
-
-            mutate("/student");
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
+const ClearanceApplicationPage = () => {
     return (
         <>
             <img
@@ -38,41 +17,6 @@ const ClearanceApplicationPage = ({ studentNumber }) => {
             </p>
             <label className="btn btn-primary" htmlFor="link-submission">
                 Apply
-            </label>
-
-            <input
-                type="checkbox"
-                id="link-submission"
-                className="modal-toggle"
-            />
-            <label className="modal cursor-pointer" htmlFor="link-submission">
-                <label className="modal-box md:px-10 md:py-8">
-                    <label
-                        htmlFor="user-profile-modal"
-                        className="btn btn-sm btn-circle btn-primary absolute right-5 top-5 text-xl"
-                    >
-                        ✕
-                    </label>
-                    <h3 className="font-bold md:text-xl text-primary mb-3">
-                        Upload Github Repository
-                    </h3>
-                    <input
-                        type="text"
-                        placeholder="Paste link here"
-                        className="input input-bordered w-full"
-                        value={link}
-                        onChange={(e) => setLink(e.currentTarget.value)}
-                    />
-                    <div className="modal-action justify-start">
-                        <label
-                            htmlFor="link-submission"
-                            className="btn btn-primary"
-                            onClick={applyApplication}
-                        >
-                            Submit
-                        </label>
-                    </div>
-                </label>
             </label>
         </>
     );
