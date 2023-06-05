96d5182956f9fe321c438fc8b7102478022354f8
gmviray <gmviray@up.edu.ph>
2023-06-05 23:07:32 +0800

    ui: responsive sign up and sign in

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -25,25 +25,25 @@ const SignUpPage = () => {
         <div className="container mx-auto px-5">
             {" "}
             <div className="hero min-h-screen">
-                <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="hero-content flex-col lg:flex-row">
                     <div className="text-center">
                         <img
-                            className="h-fit max-w-lg rounded-lg"
+                            className="w-full object-contain sm:w-auto"
                             src={signUpSvg}
                         />
-                        <h1 className="text-5xl font-bold mt-6 text-primary">
-                            Create Account
-                        </h1>
-                        <h3 className="text-min font-bold mt-6">
-                            Let's set you an account so that you now proceed to
-                            your next journey in life, getting a job.
-                        </h3>
+                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mt-4 md:mt-6 lg:mt-8 text-primary">
+  Create Account
+</h1>
+
+                        <h3 className="text-base md:text-lg lg:text-xl font-bold mt-6 md:mt-8 lg:mt-10">
+  Let's set you an account so that you can proceed to your next journey in life, getting a job.
+</h3>
                     </div>
-                    <div className="card flex-shrink-0 w-full shadow-md bg-base-100">
+                    <div className="card flex-shrink-0 shadow-md bg-base-100 flex flex-col">
                         <div className="card-body">
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <div className="flex flex-row gap-5">
-                                    <div className="basis-1/2">
+                            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+                                    
                                         <div className="form-control">
                                             <label
                                                 className="label"
@@ -57,7 +57,7 @@ const SignUpPage = () => {
                                             <input
                                                 type="text"
                                                 placeholder="First Name"
-                                                className="input input-bordered"
+                                                className="input input-bordered w-full sm:w-auto"
                                                 id="firstname"
                                                 {...register("firstname", {
                                                     required: true,
@@ -68,9 +68,9 @@ const SignUpPage = () => {
                                                     This is a required field.
                                                 </span>
                                             )}
-                                        </div>
+                                        {/* </div> */}
                                     </div>
-                                    <div className="basis-1/2">
+                                    {/* <div className="basis-1/2"> */}
                                         <div className="form-control">
                                             <label
                                                 className="label"
@@ -84,15 +84,15 @@ const SignUpPage = () => {
                                             <input
                                                 type="text"
                                                 placeholder="Middle Name"
-                                                className="input input-bordered"
+                                                className="input input-bordered  w-full sm:w-auto"
                                                 id="middlename"
                                                 {...register("midname")}
                                             />
-                                        </div>
+                                        {/* </div> */}
                                     </div>
                                 </div>
-                                <div className="flex flex-row gap-5">
-                                    <div className="basis-1/2">
+                                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+                                    {/* <div className="basis-1/2"> */}
                                         <div className="form-control">
                                             <label
                                                 className="label"
@@ -106,7 +106,7 @@ const SignUpPage = () => {
                                             <input
                                                 type="text"
                                                 placeholder="Last Name"
-                                                className="input input-bordered"
+                                                className="input input-bordered  w-full sm:w-auto"
                                                 id="lastname"
                                                 {...register("lastname", {
                                                     required: true,
@@ -117,9 +117,9 @@ const SignUpPage = () => {
                                                     This is a required field.
                                                 </span>
                                             )}
-                                        </div>
+                                        {/* </div> */}
                                     </div>
-                                    <div className="basis-1/2">
+                                    {/* <div className="basis-1/2"> */}
                                         <div className="form-control">
                                             <label
                                                 className="label"
@@ -133,7 +133,7 @@ const SignUpPage = () => {
                                             <input
                                                 type="text"
                                                 placeholder="Student Number"
-                                                className="input input-bordered"
+                                                className="input input-bordered  w-full sm:w-auto"
                                                 id="studentnum"
                                                 {...register("studentnum", {
                                                     required: true,
@@ -144,7 +144,7 @@ const SignUpPage = () => {
                                                     This is a required field.
                                                 </span>
                                             )}
-                                        </div>
+                                        {/* </div> */}
                                     </div>
                                 </div>
                                 <div className="form-control">
@@ -157,7 +157,7 @@ const SignUpPage = () => {
                                     <input
                                         type="text"
                                         placeholder="UP Mail"
-                                        className="input input-bordered"
+                                        className="input input-bordered w-full sm:w-auto"
                                         id="email"
                                         {...register("emailInput", {
                                             required: true,
@@ -169,8 +169,8 @@ const SignUpPage = () => {
                                         </span>
                                     )}
                                 </div>
-                                <div className="flex flex-row gap-5">
-                                    <div className="basis-1/2">
+                                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
+                                    {/* <div className="basis-1/2"> */}
                                         <div className="form-control">
                                             <label
                                                 className="label"
@@ -184,7 +184,7 @@ const SignUpPage = () => {
                                             <input
                                                 type="password"
                                                 placeholder="Enter your password"
-                                                className="input input-bordered"
+                                                className="input input-bordered  w-full sm:w-auto"
                                                 id="password"
                                                 {...register("passwordInput", {
                                                     required: true,
@@ -195,9 +195,9 @@ const SignUpPage = () => {
                                                     This is a required field.
                                                 </span>
                                             )}
-                                        </div>
+                                        {/* </div> */}
                                     </div>
-                                    <div className="basis-1/2">
+                                    {/* <div className="basis-1/2"> */}
                                         <div className="form-control">
                                             <label
                                                 className="label"
@@ -211,7 +211,7 @@ const SignUpPage = () => {
                                             <input
                                                 type="password"
                                                 placeholder="Enter your password"
-                                                className="input input-bordered"
+                                                className="input input-bordered  w-full sm:w-auto"
                                                 id="confirm"
                                                 {...register("confirmPW", {
                                                     required: true,
@@ -222,7 +222,7 @@ const SignUpPage = () => {
                                                     This is a required field.
                                                 </span>
                                             )}
-                                        </div>
+                                        {/* </div> */}
                                     </div>
                                 </div>
 
